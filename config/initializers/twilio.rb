a545320118c93b359016a9a8706ee060ca7c577a
Travis <foggiest_shadow@Traviss-Air.lan>
2023-09-11 01:19:23 -0500

    Blackbeard initial commit

---
+++ config/initializers/twilio.rb
@@ -0,0 +1,6 @@
+require 'twilio-ruby'
+
+Twilio.configure do |config|
+    config.account_sid = 'ACd546fb125ee2a5b823765d3e82714dfc'
+    config.auth_token  = '5a66392575535b7b3a88e22aadc8cd9d'
+end
